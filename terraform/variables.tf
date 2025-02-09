ca026793cc2e9c7e25874136bcda7979693161b2
Itserge1 <realitserge@gmail.com>
2025-02-09 13:25:16 -0500

    add terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "us-east-1a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "172.56.164.238/32"
+}
+variable jenkins_ip {
+    default = "167.71.251.96/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable = {
+    default = "us-east-1"
+}
